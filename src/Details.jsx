66b55fbe6de6fe507c2b9f068cedf7648b392043
marik <letschuka@gmail.com>
2024-03-21 11:31:46 +0200

    add conf files

--- src/Details.jsx
+++
@@ -1,9 +0,0 @@
-export function Details({title, setTitle}) {
-    return (
-        <>
-        <h1>{title}</h1>
-        <div>Hello, my world !!!</div>
-        <button onClick={() => setTitle(prev => prev + 'new')}>Click</button>
-        </>
-    )
-}
