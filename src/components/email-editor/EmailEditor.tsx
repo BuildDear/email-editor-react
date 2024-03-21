4eac590507366e60e311256d088ce4d6fba7f334
marik <letschuka@gmail.com>
2024-03-21 15:57:25 +0200

    add ty[ing to function

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,7 +1,28 @@
 import { Bold, Eraser, Italic, Underline } from 'lucide-react'
 import styles from  './EmailEditor.module.scss'
+import { useRef, useState } from 'react'
 
 export function EmailEditor() {
+
+  const [text, setText] = useState(`Hey, Marik Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, amet? Nulla expedita possimus pariatur corrupti, ullam magni, dolores earum impedit minus sed, et velit animi dolorum eveniet temporibus ad nostrum.`)
+  const textRef = useRef<HTMLTextAreaElement | null>(null)
+
+  const getSelectionText = (): string | undefined => {
+    if (!textRef.current) return undefined;
+
+    const cursorStart: number | undefined = textRef.current?.selectionStart;
+    const cursorEnd: number | undefined = textRef.current?.selectionEnd;
+
+    if (cursorStart === undefined || cursorEnd === undefined) return undefined;
+
+    const selectedText: string = text.substring(cursorStart, cursorEnd);
+
+    if (!selectedText) return undefined;
+
+    return selectedText;
+}
+
+
   
   return (
   <div>
@@ -9,18 +30,33 @@ export function EmailEditor() {
 
     <div className={styles.card}>
       
-      <textarea className={styles.editor} spellCheck='false'>
-      Hey, Marik
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, amet? Nulla expedita possimus pariatur corrupti, ullam magni, dolores earum impedit minus sed, et velit animi dolorum eveniet temporibus ad nostrum.
+      <textarea
+      ref={textRef} 
+      contentEditable 
+      className={styles.editor} 
+      spellCheck='false'
+      onClick={getSelectionText}
+      value={text}
+      onChange={e => setText(e.target.value)}
+      >
+
       </textarea>
 
       <div className={styles.actions}>
 
         <div className={styles.tools}>
-          <button><Eraser size={17}/></button>
-          <button><Bold size={17}/></button>
-          <button><Italic size={17}/></button>
-          <button><Underline size={17}/></button>
+          <button onClick={() => setText('')}>
+            <Eraser size={17}/>
+          </button>
+          <button>
+            <Bold size={17}/>
+          </button>
+          <button>
+            <Italic size={17}/>
+          </button>
+          <button>
+            <Underline size={17}/>
+          </button>
         </div>
 
         <button className={styles.send_button}>Send now</button>
